786b0b4e5f724ad0bda0074f556156e276598a6b
rai.ekikun <rai.ekikun@techcross.co.jp>
2018-10-04 20:19:31 +0900

    feature: スーパーTest01追加

--- tests/Test01.php
+++ tests/Test01.php
@@ -5,6 +5,29 @@ use League\Csv\Reader;
 class Test01 extends \TestCase
 {
 	public function test01(){
-		$csvReader = Reader::createFromPath( self::csv_file_path,'r');
+
+		parent::setUp();
+
+		$repository = new \app\Domain\Achievements\AccountAchievementRepository();
+
+
+		$AchievementsCollection = $repository->getAll();
+		$achievements = $AchievementsCollection->first();
+
+		/** @var \app\Domain\Achievements\AccountAchievements $achievements */
+		var_dump('id = '.$achievements->getAccountId());
+		var_dump($achievements->getNickName());
+
+		/** @var \app\Domain\Achievements\Achievement $achievement */
+		$achievement = $achievements->getAchievements()->first();
+		var_dump($achievement->getAchievementName());
+		var_dump($achievement->getAchievementContent());
+		dd(24);
+
 	}
+
+
+
+
+
 }
